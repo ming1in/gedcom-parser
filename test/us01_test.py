ee005bd00ed2f46c69ace0d0ce25e9489c9ba3d2
Michael McCreesh <mmccree1@stevens.edu>
2020-10-05 19:50:20 -0400

    adding stuff

---
+++ test/us01_test.py
@@ -0,0 +1,25 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.abspath('../src'))
+import us01
+
+# ----------------------------------- Test Cases for Use Case 07 -----------------------------------
+class usecase01_test(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(us01.usecase01("seeds/test8.ged"),'All dates are before today')
+
+    def test2(self):
+            self.assertEqual(us01.usecase01("seeds/test9.ged"),'All dates are before today')
+
+    def test3(self):
+            self.assertEqual(us01.usecase01("seeds/test10.ged"),'All dates are before today')
+
+    def test4(self):
+            self.assertEqual(us01.usecase01("seeds/test11.ged"),"There is at least one date later than the current date")
+
+    def test5(self):
+            self.assertEqual(us01.usecase01("seeds/test12.ged"),"There is at least one date later than the current date")
+
+if __name__ == '__main__':
+    unittest.main()
