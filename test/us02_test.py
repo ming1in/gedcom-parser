89881d6f06c1278c33d1cd1c7511db1fdda07bd3
gracemiguel <gracem730@gmail.com>
2020-10-05 21:32:37 -0400

    path and change location

---
+++ test/us02_test.py
@@ -0,0 +1,31 @@
+import sys
+import unittest
+import os
+sys.path.append(os.path.abspath('../test/seeds'))
+sys.path.append(os.path.abspath('../src'))
+import us02
+
+
+class Testuser02(unittest.TestCase):
+    #all marriages after birth
+
+    def test01(self):
+        s = ""
+        self.assertEquals(s, us02.us02("seeds/test1.ged"))
+
+    def test02(self):
+        s = ""
+        self.assertEquals(s, us02.us02("seeds/test2.ged"))
+
+    def test03(self):
+        s = ""
+        self.assertEquals(s, us02.us02("seeds/test3.ged"))
+    def test04(self):
+        s = ""
+        self.assertEquals(s, us02.us02("seeds/test4.ged"))
+    def test05(self):
+        s = ""
+        self.assertEquals(s, us02.us02("seeds/test5.ged"))
+
+if __name__ == '__main__':
+    unittest.main()
