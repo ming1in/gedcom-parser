8e527fe574804419b728e90662b2bc41f3b8e189
Michael McCreesh <mmccree1@stevens.edu>
2020-10-25 18:24:47 -0400

    US15, US28, and their test files

---
+++ src/us15.py
@@ -0,0 +1,21 @@
+'''
+Created on 10/24/20
+@author:   Michael McCreesh
+Pledge:    I pledge my honor to abide by the Stevens Honor System
+SSW555 - Sprint 2
+'''
+
+'''Goal: Make sure there are no more than 15 siblings per family'''
+
+from Gedcom import Gedcom, Family
+from datetime import date, datetime
+
+def us15(gedcomFile):
+    gedcom = Gedcom(gedcomFile)
+    badFams = []
+    for fam in gedcom.families:
+        if len(fam.child) > 15 :
+            badFams.append(fam.uid)
+    return badFams
+
+#print(us15("../test/seeds/seed.ged"))
