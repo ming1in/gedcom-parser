82861658a09d9f65d10963c563d4ca78dd991ad7
samanthainneo99 <43583835+samanthainneo99@users.noreply.github.com>
2020-11-07 19:57:32 -0500

    I made the folders for my seeds, but when I pull main the test_suite still doesn't work so I did not add mine because I think that will create issues

--- test/us12_test.py
+++ test/us12_test.py
@@ -3,29 +3,29 @@ import datetime
 import sys
 import os
 sys.path.append(os.path.abspath('../src'))
-import us12
+from us12 import us12
 
 class us12_test(unittest.TestCase):
     def test1(self):
-        filename = "seeds/test121.ged" 
+        filename = "seeds/us12/test121.ged" 
         ret = ['I2']
-        self.assertEqual(us12.us12(filename),ret)
+        self.assertEqual(us12(filename),ret)
 
     def test2(self):
-        filename = "seeds/seed.ged" 
+        filename = "seeds/us12/test122.ged" 
         ret = "All parents are an acceptable age"
-        self.assertEqual(us12.us12(filename),ret)
+        self.assertEqual(us12(filename),ret)
 
     def test3(self):
-        filename = "seeds/test123.ged" 
+        filename = "seeds/us12/test123.ged" 
         ret = ['I2', 'I7']
-        self.assertEqual(us12.us12(filename),ret)
+        self.assertEqual(us12(filename),ret)
 
 
     def test4(self):
-        filename = "seeds/test124.ged" 
+        filename = "seeds/us12/test124.ged" 
         ret = ['I6']
-        self.assertEqual(us12.us12(filename),ret)
+        self.assertEqual(us12(filename),ret)
 
 
 
--- test/us46_test.py
+++ test/us46_test.py
@@ -3,30 +3,30 @@ import datetime
 import sys
 import os
 sys.path.append(os.path.abspath('../src'))
-import us46
+from us46 import us46
 
 class us46_test(unittest.TestCase):
     def test1(self):
-        filename = "seeds/seed.ged" 
+        filename = "seeds/us46/seed.ged" 
         ret = [['I11'], ['I13']]
-        self.assertEqual(us46.us46(filename),ret)
+        self.assertEqual(us46(filename),ret)
 
     def test2(self):
-        filename = "seeds/test7.ged" 
+        filename = "seeds/us46/test7.ged" 
         ret = [['I4'], ['I8']]
-        self.assertEqual(us46.us46(filename),ret)
+        self.assertEqual(us46(filename),ret)
 
 
     def test3(self):
-        filename = "seeds/test8.ged" 
+        filename = "seeds/us46/test8.ged" 
         ret = [['I11'], ['I13']]
-        self.assertEqual(us46.us46(filename),ret)
+        self.assertEqual(us46(filename),ret)
 
 
     def test4(self):
-        filename = "seeds/test3.ged" 
+        filename = "seeds/us46/test3.ged" 
         ret = "No only children"
-        self.assertEqual(us46.us46(filename),ret)
+        self.assertEqual(us46(filename),ret)
 
 
 
