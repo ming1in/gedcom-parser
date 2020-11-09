2e29d803c83017654437b252e4eff13cd938585a
gracemiguel <gracem730@gmail.com>
2020-11-08 21:45:54 -0500

    update

--- src/us06.py
+++ src/us06.py
@@ -8,27 +8,23 @@ import copy
 from datetime import datetime
 import pandas as pd 
 import Project02
-os.chdir(os.path.abspath('../test/seeds'))
+os.chdir(os.path.abspath('test/seeds'))
 #This function is used to determine if the Divorce date is after the Death date
 def us06(gedcom_file):
      individuals = Project02.createIndividualsDataFrame(gedcom_file)
      families = Project02.createFamiliesDataFrame(gedcom_file)
      indivs = copy.deepcopy(individuals[['Dead', 'Name']])
      fam = copy.deepcopy(families[['Husband Name', 'Wife Name', 'Divorced']])
-     incorrect = []  
+     incorrect = []
+    
      for i, row in fam.iterrows():
          for k, rows in indivs.iterrows():
              if row['Wife Name']==rows['Name']:
                  if type(row['Divorced'])==float and pd.isna(row['Divorced']):
-                     pass
+                     continue
                  elif pd.to_datetime(row['Divorced']) > pd.to_datetime(rows['Dead']):
                      incorrect.append(row["Wife Name"])
-             if row['Husband Name']==rows['Name']:
-                 if type(row['Divorced'])==float and pd.isna(row['Divorced']):
-                     pass
-                 elif pd.to_datetime(row['Divorced']) > pd.to_datetime(rows['Dead']):
                      incorrect.append(row["Husband Name"])
-                    
      if len(incorrect)>0:
-        print("The following people have divorces after their death date" + str(incorrect))
+        return "The following people have divorces after their death date" , str(incorrect)
 
--- test/us06_test.py
+++ test/us06_test.py
@@ -1,21 +1,20 @@
 import unittest
 import os
 import sys
-sys.path.append(os.path.abspath('../src'))
+sys.path.append(os.path.abspath('src'))
+
 import us06
-
-
 
 
 
 class us06_test(unittest.TestCase):
     def test1(self):
-        filename = "us06/test1.ged"
-        s = "The following people have divorces after their death date['DANIEL SOLLER']"
+        filename = "test19.ged"
+        s = None
         self.assertEqual(s, us06.us06(filename))
     
     def test2(self):
-        filename = "us06/test2.ged"
+        filename = "test2.ged"
         s = None
         self.assertEqual(s, us06.us06(filename))
 
